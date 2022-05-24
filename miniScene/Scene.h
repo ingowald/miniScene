0840e033647c6c76e6b385b35319502af9e6e271
Ingo Wald <ingowald@gmail.com>
2022-05-24 14:16:50 -0600

    merged texture in Scene.h

--- miniScene/Scene.h
+++ miniScene/Scene.h
@@ -16,10 +16,32 @@
 
 #pragma once
 
-#include "Material.h"
+#include "miniScene/common.h"
 
 namespace mini {
     
+  /*! a object for storing texture data; allowing to store both image
+      textures and raw data for 'embedded' ptex texture (where
+      'embedded' means that this stores the same data as the .ptex
+      file, but without any claim to being able to do something with
+      it) */
+  struct Texture {
+    typedef std::shared_ptr<Texture> SP;
+
+    static SP create() { return std::make_shared<Texture>(); }
+      
+    typedef enum {
+                  UNDEFINED=0, EMBEDDED_PTEX, FLOAT4, FLOAT1, RGBA_UINT8, BYTE4=RGBA_UINT8, 
+    } Format;
+      
+    /* for embedded ptex, size is always {0,0}, since data vector
+       containst the raw ptex fiel content; for all others it is the
+       number of pixels in x and y */
+    vec2i  size    { 0, 0 };
+    Format format  { UNDEFINED };
+    std::vector<uint8_t> data;
+  };
+
   struct Material {
     typedef std::shared_ptr<Material> SP;
 
