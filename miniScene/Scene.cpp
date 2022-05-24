9d5660feafb910917a7faec40a9d8d2508b63b91
Ingo Wald <ingowald@gmail.com>
2022-05-24 14:33:09 -0600

    a few mroe comments and cleanups

--- miniScene/Scene.cpp
+++ miniScene/Scene.cpp
@@ -40,19 +40,6 @@ namespace mini {
     return bounds;
   }
     
-  box3f  Mesh::getPrimBounds(size_t primID,
-                             const AffineSpace3f &xfm) const
-  {
-    const vec3i idx = indices[primID];
-    const vec3f A = xfmPoint(xfm,vertices[idx.x]);
-    const vec3f B = xfmPoint(xfm,vertices[idx.y]);
-    const vec3f C = xfmPoint(xfm,vertices[idx.z]);
-    return box3f()
-      .including(A)
-      .including(B)
-      .including(C);
-  }
-
   box3f Object::getBounds() const
   {
     box3f bounds;
