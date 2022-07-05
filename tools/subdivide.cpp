94c3b7d15c7a34c81c2cfa35b67dc7bdb3fb8c3a
Celeste Zepeda <celestezepeda78@gmail.com>
2022-07-05 10:04:29 -0700

    subdivide tool update

--- tools/subdivide.cpp
+++ tools/subdivide.cpp
@@ -21,175 +21,154 @@
 
 namespace mini {
 
-  void usage(const std::string &error = "")
-  {
-    if (!error.empty())
-      std::cerr << MINI_COLOR_RED << "Error: " << error
-                << MINI_COLOR_DEFAULT << std::endl << std::endl;
-    std::cout << "miniSubdivide a.mini b.mini ... -o subdivided.mini" << std::endl;
-    exit(error.empty()?0:1);
-  }
+    void usage(const std::string& error = "")
+    {
+        if (!error.empty())
+            std::cerr << MINI_COLOR_RED << "Error: " << error
+            << MINI_COLOR_DEFAULT << std::endl << std::endl;
+        std::cout << "miniSubdivide a.mini b.mini ... -o subdivided.mini" << std::endl;
+        exit(error.empty() ? 0 : 1);
+    }
 
-  std::string midpointIndexToString(int32_t i1, int32_t i2)
-  {
-      if (i1 < i2)
-          return std::to_string(i1) + "," + std::to_string(i2);
-      else
-          return std::to_string(i2) + "," + std::to_string(i1);
-  }
+    std::string midpointIndexToString(int32_t i1, int32_t i2)
+    {
+        if (i1 < i2)
+            return std::to_string(i1) + "," + std::to_string(i2);
+        else
+            return std::to_string(i2) + "," + std::to_string(i1);
+    }
 
-  
-  void miniSubdivide(int ac, char** av)
-  {
-      
-      std::string outFileName = "";
-      
-      if (ac == 1) usage();
-      std::string inFileName = "";
-      for (int i = 1; i < ac; i++) {
-          std::string arg = av[i];
-          if (arg[0] != '-')
-              inFileName = arg;
-          else if (arg == "-o")
-              outFileName = av[++i];
-          else
-               usage("unknown cmdline argument '" + arg + "'");
-      }
+    void miniSubdivide(int ac, char** av)
+    {
+        std::string outFileName = "";
 
-      if (inFileName.empty())
-          usage("no input file names specified");
-      if (outFileName.empty())
-          usage("no output file name specified");
+        //*putting in places info from command line
+        if (ac == 1) usage();
+        std::string inFileName = "";
+        for (int i = 1; i < ac; i++) {
+            std::string arg = av[i];
+            if (arg[0] != '-')
+                inFileName = arg;
+            else if (arg == "-o")
+                outFileName = av[++i];
+            else
+                usage("unknown cmdline argument '" + arg + "'");
+        }
 
-      try {
+        if (inFileName.empty())
+            usage("no input file names specified");
+        if (outFileName.empty())
+            usage("no output file name specified");
 
-          std::cout << "Creating scene" << std::endl;
+        try {
+            std::cout << "Creating scene \n";
 
-         // Scene::SP out = Scene::create();
+            std::cout << MINI_COLOR_LIGHT_BLUE
+                << "Loading mini file from " << inFileName
+                << MINI_COLOR_DEFAULT << std::endl;
 
-          std::cout << MINI_COLOR_LIGHT_BLUE
-              << "loading mini file from " << inFileName
-              << MINI_COLOR_DEFAULT << std::endl;
+            Scene::SP scene = Scene::load(inFileName);
 
-          Scene::SP scene = Scene::load(inFileName);
+            std::vector<Instance::SP> newInstances;
+            for (auto& inst : scene->instances)
+            {
+                std::vector<Mesh::SP> newMeshes;
+                for (auto& mesh : inst->object->meshes)
+                {
+                    std::map<std::string, int32_t> vertexMap;
+                    Mesh::SP newMesh = Mesh::create(mesh->material);
+                    std::vector<vec3f> vertices;
+                    std::vector<vec3i> indices;
+                    for (auto& index : mesh->indices)
+                    {
+                        int32_t i[3]; // indices for original vertices
+                        int32_t j[3]; // indices for midpoints
+                        vec3f v[3];   // original vertices
+                        vec3f u[3];   // midpoints
+                        // get original vertices
+                        for (int k = 0; k < 3; k++)
+                        {
+                            i[k] = index[k];
+                            v[k] = mesh->vertices[i[k]];
+                        }
+                        // calculate midpoints and add to 'vertices'
+                        for (int k = 0; k < 3; k++)
+                        {
+                            int i1 = k;
+                            int i2 = (k + 1) % 3;
+                            for (int l = 0; l < 3; l++)
+                                u[k][l] = (v[i1][l] + v[i2][l]) / 2.0f;
+                            std::string indexString = midpointIndexToString(i[i1], i[i2]);
+                            if (vertexMap.find(indexString) != vertexMap.end())
+                            {
+                                j[k] = vertexMap[indexString];
+                            }
+                            else
+                            {
+                                j[k] = vertices.size();
+                                vertexMap[indexString] = j[k];
+                                vertices.push_back(u[k]);
+                            }
+                        }
+                        // add vertices to 'vertices'
+                        for (int k = 0; k < 3; k++)
+                        {
+                            std::string indexString = std::to_string(i[k]);
+                            if (vertexMap.find(indexString) != vertexMap.end())
+                            {
+                                i[k] = vertexMap[indexString];
+                            }
+                            else
+                            {
+                                i[k] = vertices.size();
+                                vertexMap[indexString] = i[k];
+                                vertices.push_back(v[k]);
+                            }
+                        }
+                        // add 4 subtriangles to 'indices'
+                        for (int k = 0; k < 3; k++)
+                        {
+                            vec3i newIndex;
+                            newIndex[0] = i[k];
+                            newIndex[1] = j[k];
+                            newIndex[2] = j[(k + 2) % 3];
+                            indices.push_back(newIndex);
+                        }
+                        vec3i newIndex;
+                        newIndex[0] = j[0];
+                        newIndex[1] = j[1];
+                        newIndex[2] = j[2];
+                        indices.push_back(newIndex);
+                    }
+                    newMesh->vertices = vertices;
+                    newMesh->indices = indices;
+                    std::cout << "Original: vertices=" << mesh->vertices.size() << ", "
+                        << "triangles=" << mesh->indices.size() << std::endl;
+                    std::cout << "New: vertices=" << newMesh->vertices.size() << ", "
+                        << "triangles=" << newMesh->indices.size() << std::endl;
+                    newMeshes.push_back(newMesh);
+                }
+                newInstances.push_back(Instance::create(Object::create(newMeshes)));
+            }
+            Scene::SP newScene = Scene::create(newInstances);
 
+            std::cout << "saving scene \n";
+            newScene->save(outFileName);
 
-          //subdivision
+            std::cout << MINI_COLOR_LIGHT_GREEN
+                << "#miniInfo: subdivided scene saved."
+                << MINI_COLOR_DEFAULT << std::endl;
 
-          for (auto& inst : scene->instances)
-          {
-              for (auto& mesh : inst->object->meshes)
-              {
-                  // Create a new mesh
-                  std::map<std::string, int32_t> vertexMap;
-                  Mesh::SP newMesh = Mesh::create(mesh->material);
-                  std::vector<vec3f> vertices;
-                  std::vector<vec3i> indices;
-                  for (auto& index : mesh->indices)
-                  {
-                      int32_t i[3]; // indices for original vertices
-                      int32_t j[3]; // indices for midpoints
-                      vec3f v[3];   // original vertices
-                      vec3f u[3];   // midpoints
-                      // get original vertices
-                      for (int k = 0; k < 3; k++)
-                      {
-                          i[k] = index[k];
-                          v[k] = mesh->vertices[i[k]];
-                      }
-                      // calculate midpoints and add to 'vertices'
-                      for (int k = 0; k < 3; k++)
-                      {
-                          int i1 = k;
-                          int i2 = (k + 1) % 3;
-                          for (int l = 0; l < 3; l++)
-                              u[k][l] = (v[i1][l] + v[i2][l]) / 2.0f;
-                          std::string indexString = midpointIndexToString(i[i1], i[i2]);
-                          if (vertexMap.find(indexString) != vertexMap.end())
-                          {
-                              j[k] = vertexMap[indexString];
-                          }
-                          else
-                          {
-                              j[k] = vertices.size();
-                              vertexMap[indexString] = j[k];
-                              vertices.push_back(u[k]);
-                          }
-                      }
-                      // add vertices to 'vertices'
-                      for (int k = 0; k < 3; k++)
-                      {
-                          std::string indexString = std::to_string(i[k]);
-                          if (vertexMap.find(indexString) != vertexMap.end())
-                          {
-                              i[k] = vertexMap[indexString];
-                          }
-                          else
-                          {
-                              i[k] = vertices.size();
-                              vertexMap[indexString] = i[k];
-                              vertices.push_back(v[k]);
-                          }
-                      }
-                      // add 4 subtriangles to 'indices'
-                      for (int k = 0; k < 3; k++)
-                      {
-                          vec3i newIndex;
-                          newIndex[0] = i[k];
-                          newIndex[1] = j[k];
-                          newIndex[2] = j[(k + 2) % 3];
-                          indices.push_back(newIndex);
-                      }
-                      vec3i newIndex;
-                      newIndex[0] = j[0];
-                      newIndex[1] = j[1];
-                      newIndex[2] = j[2];
-                      indices.push_back(newIndex);
-                  }
-                  newMesh->vertices = vertices;
-                  newMesh->indices = indices;
-                  std::cout << "Original: vertices=" << mesh->vertices.size() << ", " <<
-                      "triangles=" << mesh->indices.size() << std::endl;
-                  std::cout << "New: vertices=" << newMesh->vertices.size() << ", " <<
-                      "triangles=" << newMesh->indices.size() << std::endl;
-
-                  // Create a new object
-                  std::vector<Mesh::SP> newMeshes;
-                  newMeshes.push_back(mesh);
-                  Object::SP newObject = Object::create(newMeshes);
-
-                  // Create a new instance
-                  Instance::SP newInstance = Instance::create(newObject);
-
-                  // Create a new scene
-                  std::vector<Instance::SP> newInstances;
-                  newInstances.push_back(newInstance);
-                  Scene::SP newScene = Scene::create(newInstances);
-
-                  std::cout << "saving scene \n";
-                  newScene->save(outFileName);
-
-                  break;
-              }
-              break;
-          }
-
-         
-          std::cout << MINI_COLOR_LIGHT_GREEN
-              << "#miniInfo: subdivided scene saved."
-              << MINI_COLOR_DEFAULT << std::endl;
-
-      }
-
-      catch (const std::exception & exc) {
-          std::cerr << exc.what();
-      }
-     
-      
-  }
+        }
+        catch (const std::exception& exc)
+        {
+            std::cerr << exc.what();
+        }
+    }
 } // ::mini
 
 int main(int ac, char** av)
 {
     mini::miniSubdivide(ac, av); return 0;
 }
+
